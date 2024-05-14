1b10a99b3aac7f4f751aca8c7312e057a87c18d2
HypothesisTester <kev34molloy@gmail.com>
2024-05-14 15:49:46 +0000

    14 May 2024, 16:49

--- dynamic_array/mechanics_of.py
+++ dynamic_array/mechanics_of.py
@@ -8,10 +8,11 @@ def pushback(self, n):
     self.length += 1
 
 def resize(self):
+    # Create new array of double capacity
     self.capacity = 2 * self.capacity
     newArr = [0] * self.capacity
     
-    
+    # Copy elements to newArr
     for i in range(self.length):
         newArr[i] = self.arr[i]
     self.arr = newArr
