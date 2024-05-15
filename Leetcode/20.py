22a60eeda1ee6ad7d4e1ac6cc0cc7c87bd5ed83c
HypothesisTester <kev34molloy@gmail.com>
2024-05-15 20:07:09 +0000

    15 May 2024, 21:07

--- Leetcode/20.py
+++ Leetcode/20.py
@@ -5,3 +5,11 @@ class Solution:
         
         for c in s:
             if c in closeToOpen:
+                if stack and stack[-1] == closeToOpen[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+            
+            return True if not stack else False
