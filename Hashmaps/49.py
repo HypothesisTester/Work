01fdcbdb29c74ee06b0dc9aa86b9d8bad9a3e0a6
HypothesisTester <kev34molloy@gmail.com>
2024-05-16 09:53:05 +0000

    16 May 2024, 10:53

--- Hashmaps/49.py
+++ Hashmaps/49.py
@@ -1,9 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res = { } # mapping charCount to list of Anagrams
+        res = defaultdict(list) # mapping charCount to list of Anagrams
     
         for s in strs:
             count = [0] * 26 # a ... z
             
             for c in s:
-                count[ard(c) - ard("a")]
+                count[ard(c) - ard("a")] += 1
+                
+            result[tuple(count)].append(s)
+        
+        return result.values()
