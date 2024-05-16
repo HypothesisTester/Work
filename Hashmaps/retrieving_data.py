dc2c3f6fea1fedc69a24b8519fbde5b8662fd42c
HypothesisTester <kev34molloy@gmail.com>
2024-05-16 09:30:32 +0000

    16 May 2024, 10:30

---
+++ Hashmaps/retrieving_data.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+
+hashmap.keys()
+# returns all of the keys from the dict in the form a a list
+    
+hashmap.values()
+# returns all of the values from the dict in the form a a list
+
+hashmap.items()
+# combination of the two, it returns a list of all of the key value pairs as tuples
