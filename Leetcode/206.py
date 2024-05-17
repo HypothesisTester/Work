d71579b5466a91081e258ea435b592b7bba900ac
HypothesisTester <kev34molloy@gmail.com>
2024-05-17 22:52:44 +0000

    17 May 2024, 23:52

--- Leetcode/206.py
+++ Leetcode/206.py
@@ -5,4 +5,15 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # recursive: T O(n), M O(n)
         
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+        
+        return newHead
