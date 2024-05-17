ce4345cd3cb3a592f708f31478d6e0cbfb10954e
HypothesisTester <kev34molloy@gmail.com>
2024-05-17 16:59:13 +0000

    17 May 2024, 17:59

--- Leetcode/155.py
+++ Leetcode/155.py
@@ -7,8 +7,8 @@ class MinStack:
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        val = min(val, self.minStack[-1] if self.minStack else val)
-        self.minStack.append(val)
+        if not self.minStack or val <= self.minStack[-1]:
+            self.minStack.append(val)
 
     def pop(self) -> None:
         self.stack.pop()
