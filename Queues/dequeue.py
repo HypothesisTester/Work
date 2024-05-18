ef507bb607a096091f495e28366ea6c04c4c3f90
HypothesisTester <kev34molloy@gmail.com>
2024-05-18 22:07:15 +0000

    18 May 2024, 23:07

---
+++ Queues/dequeue.py
@@ -0,0 +1,11 @@
+def dequeue(self):
+    # Queue is empty
+    if not self.left:
+        return None
+    
+    # Remove left node and return value
+    val = self.left.val
+    self.left = self.left.next
+    if not self.left:
+        self.right = None
+    return val
---
+++ Queues/enqueue.py
@@ -0,0 +1,10 @@
+def enqueue(self, val):
+    newNode = ListNode(val)
+    
+    # Queue is non-empty
+    if self.right:
+        self.right.next = newNode
+        self.right = self.right.next
+    # Queue is empty
+    else:
+        self.left = self.right = newNode
