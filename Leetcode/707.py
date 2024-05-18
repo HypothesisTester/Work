47dae33581bdbc23da3920f095473628e1407e8d
HypothesisTester <kev34molloy@gmail.com>
2024-05-18 22:23:03 +0000

    18 May 2024, 23:23

---
+++ Leetcode/707.py
@@ -0,0 +1,34 @@
+class ListNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+    
+class MyLinkedList(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.head = None
+        self.size = 0
+        
+    def get(self, index: int) -> int:
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        """
+        if index < 0 or index >= self.size:
+            return -1
+        
+        current = self.head
+        
+        for _ in range(0, index):
+            current = current.next
+        
+        return current.val
+    
+    def addAtHead(self, val: int) -> None:
+        """
+        Add a node of value val before the first element of the linked list. After the insertion, the new node will be
+        the first node of the linked list.
+        """
+        self.addAtIndex(0, val)
+        
