b0980632502206ab09b2fb36619dab4dff5359c2
HypothesisTester <kev34molloy@gmail.com>
2024-05-19 22:54:15 +0000

    19 May 2024, 23:54

--- Leetcode/1472.py
+++ Leetcode/1472.py
@@ -8,4 +8,11 @@ class BrowserHistory:
     def back(self, steps: int) -> str:
         
     
-    def forward(self, steps: int) -> str
+    def forward(self, steps: int) -> str:
+        
+    
+    # Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
