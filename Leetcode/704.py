c803ff9f0a27c7e5a76cf6536024509a19e3f778
HypothesisTester <kev34molloy@gmail.com>
2024-05-21 22:48:27 +0000

    21 May 2024, 23:48

--- Leetcode/704.py
+++ Leetcode/704.py
@@ -5,4 +5,7 @@ class Solution:
         while l <= r:
             m = l + ((r - l) // 2)  
             # (l + r) // 2 can lead to overflow
-            
+            if nums[m] > target:
+                r = m - 1
+                elif nums[m] < target:
+                l = m + 1
