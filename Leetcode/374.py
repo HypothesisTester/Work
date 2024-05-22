a2210e83e30d8a01df00975b0f22816ef7e47e0a
HypothesisTester <kev34molloy@gmail.com>
2024-05-22 22:59:12 +0000

    22 May 2024, 23:59

--- Leetcode/374.py
+++ Leetcode/374.py
@@ -20,3 +20,23 @@ class Solution:
             myGuess = (lowerBound+upperBound) >> 1
         
         return myGuess
+    
+# Or
+
+def guessNumber(self, n: int) -> int:
+	low = 0
+	high = n
+	while low <= high:
+            mid = (low + high ) // 2
+            res = guess(mid)
+            if res < 0:
+			    high = mid - 1
+            
+            elif res > 0:
+			    low = mid + 1
+        
+            else:
+                return mid
+            
+            
+            
