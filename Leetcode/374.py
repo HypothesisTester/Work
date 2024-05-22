93d03f056177e8685c68dbd037803a3e00ebe79a
HypothesisTester <kev34molloy@gmail.com>
2024-05-22 22:54:37 +0000

    22 May 2024, 23:54

--- Leetcode/374.py
+++ Leetcode/374.py
@@ -9,3 +9,6 @@ class Solution:
     def guessNumber(self, n: int) -> int:
         lowerBound, upperBound = 1, n
         # Binary division faster than (lowerBound + upperBound) //2
+        myGuess = (lowerBound+upperBound) >> 1
+        # walrus operator ':=' - assigns value of the function to the variable 'res'
+        # and then compare res with 0
