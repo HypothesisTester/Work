0d40c7b3e773746468ae1465c081bc9904a96fd3
HypothesisTester <kev34molloy@gmail.com>
2024-05-22 23:01:39 +0000

    23 May 2024, 00:01

--- Leetcode/374.py
+++ Leetcode/374.py
@@ -97,3 +97,18 @@ class Solution:
                 pick_range[1] = pick - 1
             else:
                 return pick
+            
+            
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        l=0
+        r=n
+        while l<=r:
+            m=(l+r)>>1
+            if guess(m)==0:
+                return m
+            elif guess(m)==-1:
+                r=m-1
+            else:
+                l=m+1
+        
