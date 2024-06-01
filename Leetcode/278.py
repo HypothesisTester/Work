2176573d72dc13dcd74a2696761f4f40ae7bb0b1
HypothesisTester <kev34molloy@gmail.com>
2024-06-01 22:59:53 +0000

    1 Jun 2024, 23:59

--- Leetcode/278.py
+++ Leetcode/278.py
@@ -29,3 +29,33 @@ class Solution:
                     result = mid
                        
 		return result
+        
+            if (isBadVersion(pivot)):
+            j = pivot       # keep track of the leftmost bad version
+        else:
+            i = pivot + 1   # the one after the rightmost good version
+    return i   
+
+# Or
+
+class Solution:
+	def firstBadVersion(self, n: int) -> int:
+        
+        result = 1
+        start, end = 1, n
+             
+         while start <= end:
+                
+                mid = (start + end) // 2
+                  
+                if isBadVersion(mid) == False:
+                    start = mid + 1
+                                
+                else:
+                    end = mid - 1
+                    result = mid
+                       
+		return result
+        
+        
+        
