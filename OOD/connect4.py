c0c1861b931a559d5ba0338263353827bfda2241
HypothesisTester <kev34molloy@gmail.com>
2024-06-02 22:55:46 +0000

    2 Jun 2024, 23:55

--- OOD/connect4.py
+++ OOD/connect4.py
@@ -2,4 +2,12 @@ import enum
 
 class GridPosition(enum.Enum):
     EMPTY = 0,
-    YE
+    YELLOW = 1,
+    RED = 2
+    
+class Grid:
+    def __innit__(self, rows, columns):
+        self._rows = rows
+        self._columns = columns
+        self.grid = None
+        self.initGrid()
