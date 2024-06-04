6488ff550d8093530da9ca2e546af3be8e6bcd07
HypothesisTester <kev34molloy@gmail.com>
2024-06-04 22:56:15 +0000

    4 Jun 2024, 23:56

--- Leetcode/1472.py
+++ Leetcode/1472.py
@@ -1,9 +1,17 @@
+class ListNode:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        
 class BrowserHistory:
 
     def __init__(self, homepage: str):
+        self.cur = ListNode(homepage)
         
 
     def visit(self, url: str) -> None:
+        self.cur.next = ListNode(url, self.cur)
         
 
     def back(self, steps: int) -> str:
