955d762f44b3c31171533b9cd85c248d7da3e8e0
HypothesisTester <kev34molloy@gmail.com>
2024-06-05 22:56:26 +0000

    5 Jun 2024, 23:56

--- Leetcode/1700.py
+++ Leetcode/1700.py
@@ -1,3 +1,9 @@
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        res = len(students)
+        cnt = 
+        
+        
+        
+        return res
         
