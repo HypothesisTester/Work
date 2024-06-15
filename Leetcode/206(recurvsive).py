9400d40a33d54ff1d90d799a64bdcd64282bb03d
HypothesisTester <kev34molloy@gmail.com>
2024-06-15 23:38:52 +0100

    push

--- Leetcode/206(recurvsive).py
+++ Leetcode/206(recurvsive).py
@@ -6,4 +6,15 @@
 
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # recursive: T O(n), M O(n)
         
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+        
+        return newHead
